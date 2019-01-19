5361e9f1bd94dddd249ea9ce071e1cf7c423bcb2
Atan1988 <tzw0423@hotmail.com>
2019-01-19 16:07:09 -0500

    commit

---
+++ inst/set up color.R
@@ -0,0 +1,5 @@
+color_map <- tibble::tibble(
+  color_letter = c("B", "G", "U", "R", "Y", "S", "K", "I"),
+  color_name = c('Blue', 'Green', 'Gray', "Red", 'Yellow', 'Brown', 'Black', 'River'),
+  color_code = c(1, 2, 3, 4, 5, 6, 7, 99)
+)
